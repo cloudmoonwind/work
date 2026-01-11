e9de1449c604d44fe0ae2bc696b587156bc93102
cloud-moon <yxoy882@gmail.com>
2026-01-11 12:19:20 +0800

    3

--- gadget/oauth/api/auth.js
+++ gadget/oauth/api/auth.js
@@ -1,19 +1,22 @@
-export default function handler(req, res) {
+module.exports = function (req, res) {
   const { host } = req.headers;
-
   const client_id = process.env.OAUTH_CLIENT_ID;
+
   if (!client_id) {
-    return res.status(500).send("Missing OAUTH_CLIENT_ID");
+    res.statusCode = 500;
+    res.end("Missing OAUTH_CLIENT_ID");
+    return;
   }
 
   const redirect_uri = `https://${host}/api/callback`;
   const scope = "repo user";
 
   const url =
-    `https://github.com/login/oauth/authorize` +
+    "https://github.com/login/oauth/authorize" +
     `?client_id=${client_id}` +
     `&redirect_uri=${encodeURIComponent(redirect_uri)}` +
     `&scope=${encodeURIComponent(scope)}`;
 
-  res.redirect(url);
-}
+  res.writeHead(302, { Location: url });
+  res.end();
+};
--- gadget/oauth/api/callback.js
+++ gadget/oauth/api/callback.js
@@ -1,57 +1,37 @@
-export default async function handler(req, res) {
+module.exports = async function (req, res) {
   const { code } = req.query;
 
   if (!code) {
-    return res.status(400).send("No code provided");
+    res.statusCode = 400;
+    res.end("No code provided");
+    return;
   }
 
   const client_id = process.env.OAUTH_CLIENT_ID;
   const client_secret = process.env.OAUTH_CLIENT_SECRET;
 
-  if (!client_id || !client_secret) {
-    return res.status(500).send("Missing OAuth env vars");
-  }
-
-  try {
-    const response = await fetch(
-      "https://github.com/login/oauth/access_token",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json",
-        },
-        body: JSON.stringify({
-          client_id,
-          client_secret,
-          code,
-        }),
-      }
-    );
-
-    const data = await response.json();
-
-    if (!data.access_token) {
-      return res.status(500).json(data);
+  const response = await fetch(
+    "https://github.com/login/oauth/access_token",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({ client_id, client_secret, code }),
     }
+  );
 
-    const token = data.access_token;
+  const data = await response.json();
 
-    res.setHeader("Content-Type", "text/html");
-    res.send(`
-      <script>
-        window.opener.postMessage(
-          {
-            type: "authorization:github:success",
-            token: "${token}",
-            provider: "github"
-          },
-          "*"
-        );
-        window.close();
-      </script>
-    `);
-  } catch (err) {
-    res.status(500).send(err.toString());
-  }
-}
+  res.setHeader("Content-Type", "text/html");
+  res.end(`
+    <script>
+      window.opener.postMessage(
+        { token: "${data.access_token}", provider: "github" },
+        "*"
+      );
+      window.close();
+    </script>
+  `);
+};
